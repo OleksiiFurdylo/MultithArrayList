17c3c2b087dcb06b719bdabecd02a432a994d1e0
OleksiiFurdylo <furdilik@ukr.net>
2018-05-05 14:37:16 +0300

    small bugs

--- src/main/java/MArrayList.java
+++ src/main/java/MArrayList.java
@@ -11,11 +11,17 @@ import java.util.stream.Stream;
 
 public class MArrayList<E> extends ArrayList<E>{
 
-    private final Object lock = new Object();
+    //private final Object lock = new Object();
 
     //It is logically impossible for two threads to access the constructor of the same object.
+
+
     public MArrayList(int initialCapacity) {
         super(initialCapacity);
+    }
+
+    public MArrayList() {
+        super();
     }
 
     public MArrayList(Collection<? extends E> c) {
@@ -24,262 +30,266 @@ public class MArrayList<E> extends ArrayList<E>{
 
     @Override
     public void trimToSize() {
-        synchronized (lock) {
+        synchronized (this) {
             super.trimToSize();
         }
     }
 
     @Override
     public void ensureCapacity(int minCapacity) {
-        synchronized (lock) {
+        synchronized (this) {
             super.ensureCapacity(minCapacity);
         }
     }
 
     @Override
     public int size() {
-        synchronized (lock) {
+        synchronized (this) {
             return super.size();
         }
     }
 
     @Override
     public boolean isEmpty() {
-        synchronized (lock) {
+        synchronized (this) {
             return super.isEmpty();
         }
     }
 
     @Override
     public boolean contains(Object o) {
-        {
+        synchronized (this) {
             return super.contains(o);
         }
     }
 
     @Override
     public int indexOf(Object o) {
-        synchronized (lock) {
+        synchronized (this) {
             return super.indexOf(o);
         }
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        synchronized (lock) {
+        synchronized (this) {
             return super.lastIndexOf(o);
         }
     }
 
     @Override
     public Object clone() {
-        synchronized (lock) {
+        synchronized (this) {
             return super.clone();
         }
     }
 
     @Override
     public Object[] toArray() {
-        synchronized (lock) {
+        synchronized (this) {
             return super.toArray();
         }
     }
 
     @Override
     public <T> T[] toArray(T[] a) {
-        synchronized (lock) {
+        synchronized (this) {
             return super.toArray(a);
         }
     }
 
     @Override
     public E get(int index) {
-        synchronized (lock) {
+        synchronized (this) {
             return super.get(index);
         }
     }
 
     @Override
     public E set(int index, E element) {
-        synchronized (lock) {
+        synchronized (this) {
             return super.set(index, element);
         }
     }
 
     @Override
     public boolean add(E e) {
-        synchronized (lock) {
+        synchronized (this) {
             return super.add(e);
         }
     }
 
     @Override
     public void add(int index, E element) {
-        synchronized (lock) {
+        synchronized (this) {
             super.add(index, element);
         }
     }
 
     @Override
     public E remove(int index) {
-        synchronized (lock) {
+        synchronized (this) {
             return super.remove(index);
         }
     }
 
     @Override
     public boolean remove(Object o) {
-        synchronized (lock) {
+        synchronized (this) {
             return super.remove(o);
         }
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        synchronized (lock) {
+        synchronized (this) {
             return super.containsAll(c);
         }
     }
 
     @Override
     public void clear() {
-        synchronized (lock) {
+        synchronized (this) {
             super.clear();
         }
     }
 
     @Override
     public String toString() {
-        synchronized (lock) {
+        synchronized (this) {
             return super.toString();
         }
     }
 
     @Override
     protected void finalize() throws Throwable {
-        synchronized (lock) {
+        synchronized (this) {
             super.finalize();
         }
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
-        synchronized (lock) {
+        synchronized (this) {
             return super.addAll(c);
         }
     }
 
     @Override
     public boolean addAll(int index, Collection<? extends E> c) {
-        synchronized (lock) {
+        synchronized (this) {
             return super.addAll(index, c);
         }
     }
 
     @Override
     protected void removeRange(int fromIndex, int toIndex) {
-        synchronized (lock) {
+        synchronized (this) {
             super.removeRange(fromIndex, toIndex);
         }
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        synchronized (lock) {
+        synchronized (this) {
             return super.removeAll(c);
         }
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        synchronized (lock) {
+        synchronized (this) {
             return super.retainAll(c);
         }
     }
 
     @Override
     public ListIterator<E> listIterator(int index) {
-        synchronized (lock) {
+        synchronized (this) {
             return super.listIterator(index);
         }
     }
 
     @Override
     public ListIterator<E> listIterator() {
-        synchronized (lock) {
+        synchronized (this) {
             return super.listIterator();
         }
     }
 
     @Override
     public Iterator<E> iterator() {
-        synchronized (lock) {
+        synchronized (this) {
             return super.iterator();
         }
     }
 
     @Override
     public List<E> subList(int fromIndex, int toIndex) {
-        synchronized (lock) {
+        synchronized (this) {
             return super.subList(fromIndex, toIndex);
         }
     }
 
     @Override
     public boolean equals(Object o) {
-        synchronized (lock) {
+        synchronized (this) {
             return super.equals(o);
         }
     }
 
     @Override
     public int hashCode() {
-        synchronized (lock) {
+        synchronized (this) {
             return super.hashCode();
         }
     }
 
     @Override
     public void forEach(Consumer<? super E> action) {
-        synchronized (lock) {
+        synchronized (this) {
             super.forEach(action);
         }
     }
 
     @Override
     public Spliterator<E> spliterator() {
-        synchronized (lock) {
+        synchronized (this) {
             return super.spliterator();
         }
     }
 
     @Override
     public Stream<E> stream() {
-        return null;
+        synchronized (this) {
+            return null;
+        }
     }
 
     @Override
     public Stream<E> parallelStream() {
-        return null;
+        synchronized (this) {
+            return null;
+        }
     }
 
     @Override
     public boolean removeIf(Predicate<? super E> filter) {
-        synchronized (lock) {
+        synchronized (this) {
             return super.removeIf(filter);
         }
     }
 
     @Override
     public void replaceAll(UnaryOperator<E> operator) {
-        synchronized (lock) {
+        synchronized (this) {
             super.replaceAll(operator);
         }
     }
 
     @Override
     public void sort(Comparator<? super E> c) {
-        synchronized (lock) {
+        synchronized (this) {
             super.sort(c);
         }
     }
